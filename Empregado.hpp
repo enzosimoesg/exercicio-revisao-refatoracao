4d22f8f388f56168cce8f25c10897c40c55967b8
enzosimoesg <73187989+enzosimoesg@users.noreply.github.com>
2020-10-20 14:12:31 -0300

    Update Empregado.hpp

--- Empregado.hpp
+++ Empregado.hpp
@@ -1,3 +1,4 @@
+
 #ifndef EMPREGADO_H
 #define EMPREGADO_H
 
@@ -5,24 +6,12 @@
 #include <string>
 
 class Empregado {
-	
-  public:
-    double salarioHora;  
-    double quotaMensalVendas;  
 
+public:
+  std::string nome;
+  double salarioHora;
 
-    double pagamentoMes(double horasTrabalhadas) {
- 
-      double t = horasTrabalhadas;
-	  
-	  //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
-      if (horasTrabalhadas > 8) {
-        double x = horasTrabalhadas - 8;
-        t += x / 2;
-      }
-	  return t * salarioHora;
-    }
-	
+  double pagamentoMes(double horasTrabalhadas);
 };
 
 #endif

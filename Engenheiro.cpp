5252e31b71c0dc6cf95fd26db226bb5bed41dcfb
enzosimoesg <73187989+enzosimoesg@users.noreply.github.com>
2020-10-20 13:25:28 -0300

    Update Engenheiro.cpp

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -1,5 +1,14 @@
 #include <string>
+#include <string>
 #include "Empregado.hpp"
+
+class Engenheiro : public Empregado {
+
+public:
+    int projetos;
+
+    Engenheiro(std::string _nome, double _salarioHora, int _projetos);
+};
 
 class Engenheiro : public Empregado {
 

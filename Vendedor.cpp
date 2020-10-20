afbe029ba1c98298e3adf149a9aa637633495c7c
enzosimoesg <73187989+enzosimoesg@users.noreply.github.com>
2020-10-20 13:26:55 -0300

    Update Vendedor.cpp

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -1,14 +1,24 @@
 #include <string>
+#include <string>
 #include "Empregado.hpp"
 
 class Vendedor : public Empregado {
 
   public:
-    std::string nome;  	  
-	
-	double quotaTotalAnual() {
-	  return quotaMensalVendas * 12;
-	}
-	
+    double quotaMensalVendas;
+
+	double quotaTotalAnual();
+
+    Vendedor(std::string _nome, double _salarioHora, double _quotaMensalVendas);
 };
+
+Vendedor::Vendedor(std::string _nome, double _salarioHora, double _quotaMensalVendas) {
+	nome = _nome;
+	salarioHora = _salarioHora;
+	quotaMensalVendas = _quotaMensalVendas;
+}
+
+double Vendedor::quotaTotalAnual() {
+	return quotaMensalVendas * 12;
+}
 
